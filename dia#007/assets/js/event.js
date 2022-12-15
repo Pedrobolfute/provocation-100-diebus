19d5df208c80b9526d9b83cf70a728b0e16ea797
Pedro Henrique <pepenike-101@hotmail.com>
2022-12-15 11:16:45 -0300

    007-fixed

--- dia#007/assets/js/event.js
+++ dia#007/assets/js/event.js
@@ -1,32 +1,16 @@
-var menuButton = document.querySelector('.toggler');
-var panel = document.querySelector('.panel');
-var mineWin = document.querySelector('.window');
+let menuButton = document.querySelector('.toggler');
+let panel = document.querySelector('.panel');
+let mineWin= document.querySelector('.window');
 
 menuButton.addEventListener('click', (e) => {
-    panel.classList.toggle('panel_open');
-    mineWin.classList.toggle('window_hide');
+  panel.classList.toggle('panel_open');
+  mineWin.classList.toggle('window_hide');
 })
 
-var searchButton = document.querySelector('.search');
-var searchBar = document.querySelector('.header__searcing');
+let searchButton = document.querySelector('.search');
+let searchBar = document.querySelector('.header__searcing');
 
-searchButton.addEventListener('click', (e) => {
-    searchBar.classList.toggle('header__searcing_open');
-    searchBar.value = '';
-})
-var menuButton = document.querySelector('.toggler');
-var panel = document.querySelector('.panel');
-var mineWin = document.querySelector('.window');
-
-menuButton.addEventListener('click', (e) => {
-    panel.classList.toggle('panel_open');
-    mineWin.classList.toggle('window_hide');
-})
-
-var searchButton = document.querySelector('.search');
-var searchBar = document.querySelector('.header__searcing');
-
-searchButton.addEventListener('click', (e) => {
-    searchBar.classList.toggle('header__searcing_open');
-    searchBar.value = '';
+searchButton.addEventListener('click',  (e) => {
+  searchBar.classList.toggle('header__searcing_open');
+  searchBar.value = '';
 })
