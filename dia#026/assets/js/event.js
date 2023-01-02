ff89ebcc4b3e684346b329ef2954c7982832ef49
pedrobolfute <pepenike-101@hotmail.com>
2023-01-02 12:58:20 -0300

    026

---
+++ dia#026/assets/js/event.js
@@ -0,0 +1,10 @@
+//To fix
+var active = 1;
+$('.button').bind('click', function() {
+	$('.card:nth-child(' + active + ')').removeClass('active').addClass('inactive');
+	if (active == 3) {
+		active = 0;
+	}
+	active++;
+	$('.card:nth-child(' + active + ')').removeClass('inactive').addClass('active');
+});
