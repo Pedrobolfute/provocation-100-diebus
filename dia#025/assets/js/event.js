968f3401f799bcc372d3c3410f65b10d9bc5497e
pedrobolfute <pepenike-101@hotmail.com>
2023-01-06 14:05:04 -0300

    025 fixed + Bar do Tazinho

--- dia#025/assets/js/event.js
+++ dia#025/assets/js/event.js
@@ -1,9 +1,9 @@
-$('.marker').bind('click', function() {
-	$('.card').addClass('active');
-	$('.marker').addClass('inactive');
+$('.marker').bind('click', function(){
+  $('.card').addClass('active');
+  $('.marker').addClass('inactive');
 });
 
-$('.card').bind('click', function() {
-	$('.card').removeClass('active');
-	$('.marker').removeClass('inactive');
+$('.card').bind('click', function(){
+  $('.card').removeClass('active');
+  $('.marker').removeClass('inactive');
 });
