a1e98a0e256cf0b768e5bf639cd2e09d48a536e3
pedrobolfute <pepenike-101@hotmail.com>
2023-03-09 23:31:53 -0300

    092

---
+++ dia#092/assets/js/event.js
@@ -0,0 +1,15 @@
+frame = document.createElement('div');
+frame.classList.add('frame');
+document.body.appendChild(frame);
+
+for(let i = 1; i <= 16; i++){
+  const ball = document.createElement('div');
+  ball.classList.add(`ball`, `ball-${i}`);
+  frame.appendChild(ball)
+}
+
+const balls = document.querySelectorAll(ball)[i];
+for(let i = 1; i <= 16; i++){
+  
+}
+
